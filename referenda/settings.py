cc52a9517a34d8ad10620a16aadf6f739f374a6b
santifinland <santifinland@gmail.com>
2014-02-08 09:07:54 +0100

    collecstatic

--- referenda/settings.py
+++ referenda/settings.py
@@ -124,7 +124,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
