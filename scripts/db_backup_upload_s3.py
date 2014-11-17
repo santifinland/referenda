401a14589875da718d0a0f29a0cfbef250eb9f3c
santifinland <santifinland@gmail.com>
2014-11-17 18:25:25 +0100

    Add script for database backup

---
+++ scripts/db_backup_upload_s3.py
@@ -0,0 +1,12 @@
+from boto.s3.connection import S3Connection
+from boto.s3.bucket import Bucket
+from boto.s3.key import Key
+import datetime
+
+# User editable configs
+AWS_ACCESS_KEY = confiture
+AWS_SECRET_KEY = confiture
+
+# Upload daily database backup file
+date = datetime.datetime()
+print date
