a0101fc08fb580e73a311c4e9e0f8117573e6eef
Santiago Dobón Méndez Trelles <sdmt@tid.es>
2018-07-07 16:17:31 +0200

    [FEATURE] Add vote delegation tests

--- backend/referenda/test/users.js
+++ backend/referenda/test/users.js
@@ -2,9 +2,11 @@ const https = require('https');
 const assert = require('assert');
 
 var steps = require('./user-steps-library');
+var partySteps = require('./party-steps-library');
 
 
 synchronize(steps);
+synchronize(partySteps);
 
 feature('Users', function() {
 
@@ -17,6 +19,7 @@ feature('Users', function() {
     res = JSON.parse(steps.login(username, password));
     if (!res.err) {
       steps.deleteAllUsers(res.token);
+      partySteps.deleteAllParties(res.token);
     }
   });
 
@@ -48,8 +51,8 @@ feature('Users', function() {
     });
   });
 
-  scenario(' User logout', function() {
-  var token = '';
+  scenario('User logout', function() {
+    var token = '';
     given('As a logged in Referenda user', function() {
       username = 'user';
       password = 'pass';
@@ -68,8 +71,49 @@ feature('Users', function() {
     });
   });
 
+  scenario('Party vote delegation', function() {
+    var token = '';
+    var partyName = 'PartyDelegating'
+    given('As a logged in Referenda user', function() {
+      username = 'user';
+      password = 'pass';
+      steps.login(username, password);
+      token = JSON.parse(steps.login(username, password)).token;
+      res = steps.logged(token);
+      console.log(res);
+    });
+    and('A political party', function() {
+      adminToken = JSON.parse(steps.loginAsAdmin()).token;
+      partySteps.postParty(adminToken, partyName);
+    });
+    when('I delegate my vote to that party', function() {
+      steps.delegateParty(token, partyName);
+    });
+    then('My votes are delegated to that political party', function() {
+      party = JSON.parse(steps.delegatedParty(token));
+      assert.equal(party.name, partyName);
+    });
+  });
 
-
-
-
+  scenario('User vote delegation', function() {
+    var token = '';
+    var anotherUsername = 'anotherUser';
+    given('As a logged in Referenda user', function() {
+      username = 'user';
+      password = 'pass';
+      token = JSON.parse(steps.login(username, password)).token;
+    });
+    and('Another Referenda user', function() {
+      steps.register(anotherUsername, '1234', admin = false);
+    });
+    when('I delegate my vote to that user', function() {
+      steps.delegateUser(token, anotherUsername);
+    });
+    then('My votes are delegated to that another user', function() {
+      assert.equal(JSON.parse(steps.delegatedUser(token)).username, anotherUsername);
+    });
+    and('My votes are no longer delegated to any party', function() {
+      assert.equal(steps.delegatedParty(token), 'null');
+    });
+  });
 });
