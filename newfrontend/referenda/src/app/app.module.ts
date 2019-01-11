de34077e11188b431d8c2ba5db22589e1f77e4ce
santifinland <santifinland@gmail.com>
2019-01-11 23:16:54 +0100

    [FEATURE] Login with facebook

--- newfrontend/referenda/src/app/app.module.ts
+++ newfrontend/referenda/src/app/app.module.ts
@@ -55,6 +55,10 @@ let config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
     provider: new GoogleLoginProvider("928349623718-lfcmf80du5n1r2agv00hecdtoqmtpd60.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("297519443717240")
   }
 ]);
 
