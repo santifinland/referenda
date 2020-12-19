b9209e2457a6c10a82e51f233e444bb99f93fb00
san <santifinland@gmail.com>
2020-12-19 12:22:48 +0100

    [FEATURE] Add new footer

--- frontend/referenda/src/app/footer/footer.component.ts
+++ frontend/referenda/src/app/footer/footer.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -13,25 +12,5 @@ export class FooterComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit() {
-  }
-
-  onClick(event) {
-    event.preventDefault();
-    switch (event.toElement.pathname) {
-      case '/aboutus':
-        this.router.navigate(['/aboutus']);
-        break;
-      case '/cookies':
-        this.router.navigate(['/cookies']);
-        break;
-      case '/gdpr':
-        this.router.navigate(['/gdpr']);
-        break;
-      case '/developers':
-        this.router.navigate(['/developers']);
-        break;
-      default:
-    }
-    return true;
   }
 }
