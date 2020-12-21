8b3f57a21661c769bd72fdfc73f234460f5294ee
san <santifinland@gmail.com>
2020-12-21 05:49:57 +0100

    [FEATURE] Add new design to law details

--- frontend/referenda/src/app/law-detail/law-detail.component.ts
+++ frontend/referenda/src/app/law-detail/law-detail.component.ts
@@ -24,6 +24,12 @@ export class LawDetailComponent implements OnInit {
   facebook: string;
   twitter: string;
 
+  position: string;
+  yesHover: boolean;
+  noHover: boolean;
+  absHover: boolean;
+
+  official_total = 0;
   resultsCongreso = false;
 
   message: String = 'Cargando...';
@@ -34,18 +40,13 @@ export class LawDetailComponent implements OnInit {
   submitted = false;
 
   currentState = true;
-  readMore = 'Leer más';
+  readMore = false;
+  numComments = 0;
+  vote = '';
+  comments = false;
+  tab = 'all';
 
   location: string;
-
-  toggle() {
-    this.currentState = !this.currentState;
-    this.readMore = (this.currentState === true) ? 'Leer más' : 'Leer menos';
-    if (this.currentState === true) {
-      document.getElementById('nav').scrollIntoView({behavior: 'smooth'});
-    }
-  }
-
 
   constructor(
       private lawService: LawService,
@@ -64,6 +65,7 @@ export class LawDetailComponent implements OnInit {
     this.commentForm = this.formBuilder.group({
       comment: ['', Validators.required]
     });
+    this.position = 'favour';
   }
 
   get f() { return this.commentForm.controls; }
@@ -77,16 +79,28 @@ export class LawDetailComponent implements OnInit {
         law.positiveWidth   = (50 * law.positive   / (law.positive + law.negative + law.abstention)) + '%';
         law.negativeWidth   = (50 * law.negative   / (law.positive + law.negative + law.abstention)) + '%';
         law.abstentionWidth = (50 * law.abstention / (law.positive + law.negative + law.abstention)) + '%';
-        const official_total = law.official_positive + law.official_negative + law.official_abstention;
-        if (official_total > 0) {
+        this.official_total = law.official_positive + law.official_negative + law.official_abstention;
+        if (this.official_total > 0) {
           this.resultsCongreso = true;
         }
-        law.officialPositiveWidth   = (50 * law.official_positive   / official_total) + '%';
-        law.officialNegativeWidth   = (50 * law.official_negative   / official_total) + '%';
-        law.officialAbstentionWidth = (50 * law.official_abstention / official_total) + '%';
+        law.officialPositiveWidth   = (50 * law.official_positive   / this.official_total) + '%';
+        law.officialNegativeWidth   = (50 * law.official_negative   / this.official_total) + '%';
+        law.officialAbstentionWidth = (50 * law.official_abstention / this.official_total) + '%';
+        const totalVotes = law.positive + law.negative + law.abstention;
+        law.positivePercentage = totalVotes === 0 ? 0 : 100 * law.positive / totalVotes;
+        law.negativePercentage = totalVotes === 0 ? 0 : 100 * law.negative / totalVotes;
+        law.abstentionPercentage = totalVotes === 0 ? 0 : 100 * law.abstention / totalVotes;
+        this.numComments = law.comments.length;
         this.law = law;
         this.facebook = this.facebookPrefix + law.headline + this.facebookSuffix;
       });
+  }
+
+  toggle() {
+    this.readMore = !this.readMore;
+    if (!this.readMore) {
+      document.getElementById('menu').scrollIntoView({behavior: 'smooth'});
+    }
   }
 
   submitVote(slug: string, vote: number): void {
@@ -149,4 +163,57 @@ export class LawDetailComponent implements OnInit {
                   'twitter', 'width=600, height=300, top=' + height + ', left=' + width + ';');
     }
   }
+
+  logoWidth(party: string): string {
+    let width = '30px';
+    switch (party) {
+      case 'psoe':       { width = '30px'; break; }
+      case 'pp':         { width = '30px'; break; }
+      case 'vox':        { width = '50px'; break; }
+      case 'podemos':    { width = '69px'; break; }
+      case 'ciudadanos': { width = '100px'; break; }
+      case 'erc':        { width = '96px'; break; }
+      case 'jpc':        { width = '24px'; break; }
+      case 'pnv':        { width = '30px'; break; }
+      case 'bildu':      { width = '50px'; break; }
+      case 'mp':         { width = '36px'; break; }
+      case 'cup':        { width = '34px'; break; }
+      case 'cc':         { width = '37px'; break; }
+      case 'upn':        { width = '49px'; break; }
+      case 'bng':        { width = '34px'; break; }
+      case 'prc':        { width = '48px'; break; }
+      case 'te':         { width = '34px'; break; }
+      default: { break; }
+    }
+    return width;
+  }
+
+  logoHeight(party: string): string {
+    let height = '30px';
+    switch (party) {
+      case 'psoe':       { height = '29px'; break; }
+      case 'pp':         { height = '30px'; break; }
+      case 'vox':        { height = '25px'; break; }
+      case 'podemos':    { height = '17px'; break; }
+      case 'ciudadanos': { height = '25px'; break; }
+      case 'erc':        { height = '19px'; break; }
+      case 'jpc':        { height = '23px'; break; }
+      case 'pnv':        { height = '30px'; break; }
+      case 'bildu':      { height = '19px'; break; }
+      case 'mp':         { height = '23px'; break; }
+      case 'cup':        { height = '35px'; break; }
+      case 'cc':         { height = '35px'; break; }
+      case 'upn':        { height = '30px'; break; }
+      case 'bng':        { height = '37px'; break; }
+      case 'prc':        { height = '15px'; break; }
+      case 'te':         { height = '33px'; break; }
+      default: { break; }
+    }
+    return height;
+  }
+
+  showComments(): void {
+    this.comments = !this.comments;
+  }
+
 }
